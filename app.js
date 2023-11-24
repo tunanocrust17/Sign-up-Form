d2d5edd33cdc6a3cf7ab24d2cafc4b831d721730
ewhuet <ewhuet@gmail.com>
2023-11-24 17:32:14 +0200

    added in a variable to grab the confirm_password input and changed the confirmPasswordVariable so it will change the border of the input red if the password do not match

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 let password = document.getElementById('create_password').value;
 let confirmPassword = document.getElementById('confirm_password').value;
+let confirmPasswordVariable = document.getElementById('confirm_password');
 
-confirmPassword.addEventListener('keyup', () =>{
-    alert("Hello World");
+confirmPasswordVariable.addEventListener('keyup', () =>{
+    if(confirmPassword !== password){
+        document.getElementById('confirm_password').style.borderColor='red'
+    }
 })
