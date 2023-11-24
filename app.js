cfade79bb1e422944f2cdf6c47c8db477888e76c
ewhuet <ewhuet@gmail.com>
2023-11-24 14:39:55 +0200

    fixed the password mapping variables to remove #, started the function to check if the confirm password matches the create password

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-let password = document.getElementById('#create_password').value;
-let confirmPassword = document.getElementById('#confirm_password').value;
+let password = document.getElementById('create_password').value;
+let confirmPassword = document.getElementById('confirm_password').value;
 
+confirmPassword.addEventListener('keyup', () =>{
+    alert("Hello World");
+})
