e524b2c062a7889da46aa3754ed8fb948ba448bb
ewhuet <ewhuet@gmail.com>
2023-11-26 14:17:54 +0200

    added a variable for the password_error class and then replaced the dom path in the checkPwd formula for each occurance

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 let pwd = document.getElementById('create_password');
-let confirmPwd = document.getElementById('confirm_password')
+let confirmPwd = document.getElementById('confirm_password');
+let passwordMsg = document.getElementById('password_error');
 
 function checkPwd() {
     if(confirmPwd.value) {
         if(pwd.value != confirmPwd.value) {
             pwd.style.borderColor="red";
             confirmPwd.style.borderColor="red";
-            document.getElementById('password_error').innerHTML="*Passwords do not match!";
-            document.getElementById('password_error').style.color='red'
+            passwordMsg.innerHTML="*Passwords do not match!";
+            passwordMsg.style.color='red'
         } else {
             pwd.style.borderColor="green";
             confirmPwd.style.borderColor="green";
-            document.getElementById('password_error').innerHTML="Passwords match"
-            document.getElementById('password_error').style.color='green'
+            passwordMsg.innerHTML="Passwords match"
+            passwordMsg.style.color='green'
         }
     } 
 }
